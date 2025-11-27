430bbc9c3ece30290c810d635ef3701333dabe30
katsunny <carolopes2002@gmail.com>
2025-11-27 14:06:28 -0300

    first commit

---
+++ pacman.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // matriz 5 x 10
+    char mapa[5][10+1];
+
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+
+    if(f == 0){
+        printf("Erro na leitura do mapa D:\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < 5; i++){
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    for(int i = 0; i < 5; i++){
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(f);
+}
