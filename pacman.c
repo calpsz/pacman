18129bd10a1ffd230f8f50a062dca0e5abc7ea44
katsunny <carolopes2002@gmail.com>
2025-11-27 14:29:55 -0300

    alocacao dinamica de memoria para a matriz do mapa

--- pacman.c
+++ pacman.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h"
 
-int main(){
-    // matriz 5 x 10
-    char mapa[5][10+1];
+char** mapa;
+int linhas;
+int colunas;
 
+void liberamapa(){
+    for (int i = 0; i < colunas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}
+
+void lemapa(){
     FILE* f;
     f = fopen("mapa.txt", "r");
 
@@ -13,13 +22,32 @@ int main(){
         exit(1);
     }
 
+    fscanf(f, "%d %d", &linhas, &colunas);
+
+    alocamapa();
+
     for(int i = 0; i < 5; i++){
         fscanf(f, "%s", mapa[i]);
     }
+
+    fclose(f);
+}
+
+void alocamapa(){
+    mapa = malloc(sizeof(char*) * linhas);
+    for(int i = 0; i < colunas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas +1));
+    }
+}
+
+int main(){
+
+    lemapa();
 
     for(int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
 
-    fclose(f);
+    liberamapa();
+    
 }
